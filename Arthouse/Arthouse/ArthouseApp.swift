34a1e847a59e976c67ac0f3fd62a7324673ac101
Roberto Chavez <alex11@csu.fullerton.edu>
2025-07-09 11:18:51 -0700

    trial 1

---
+++ Arthouse/Arthouse/ArthouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArthouseApp.swift
+//  Arthouse
+//
+//  Created by Roberto Chavez on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ArthouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Arthouse/Arthouse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Arthouse
+//
+//  Created by Roberto Chavez on 7/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
