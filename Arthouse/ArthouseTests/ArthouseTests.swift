175a410948b5e75158540b41543a37d04a4ff7d2
Roberto Chavez <alex11@csu.fullerton.edu>
2025-07-09 11:18:51 -0700

    trial 1

---
+++ Arthouse/ArthouseTests/ArthouseTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArthouseTests.swift
+//  ArthouseTests
+//
+//  Created by Roberto Chavez on 7/9/25.
+//
+
+import Testing
+@testable import Arthouse
+
+struct ArthouseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
