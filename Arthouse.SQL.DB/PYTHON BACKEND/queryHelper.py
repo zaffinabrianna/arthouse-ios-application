77bde8eb8873b0b8b09aa9153b094da7dea0a701
Jacob Nguyen <jcnguyenlv@gmail.com>
2025-07-21 01:34:11 +0000

    7-20, backend getting done

---
+++ Arthouse.SQL.DB/PYTHON BACKEND/queryHelper.py
@@ -0,0 +1,51 @@
+#################################################
+# Assists in running queries on the SQL database
+#################################################
+
+from flask import Flask, request, jsonify
+import os, mysql.connector
+from dotenv import load_dotenv, dotenv_values
+
+
+#deal with login credentials
+load_dotenv("db.env")
+
+
+def get_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        database=os.getenv("DB_NAME"),
+        port=os.getenv("DB_PORT")
+)
+
+#runs queries for creation, updating, and deleting operations.
+
+def run_cud_query(query, attributesTuple):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(query, attributesTuple)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+# runs queries to read singular items
+def run_read_single(query, attributesTuple):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(query, attributesTuple)
+    result = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return result
+
+# runs queries to read multiple items
+def run_read_multiple(query, attributesTuple=None):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(query, attributesTuple)
+    result = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return result
